1e49584cbe362d9c7289cfb70c5bb7056653d104
dhruv5gohel <dhruv5gohel@gmail.com>
2023-05-11 18:37:01 +0530

    Initialised firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyAtHnV9n1uJ4TREG8GMI24EavhCkKMLiiU",
+    authDomain: "chatapp-11c8d.firebaseapp.com",
+    projectId: "chatapp-11c8d",
+    storageBucket: "chatapp-11c8d.appspot.com",
+    messagingSenderId: "712914050702",
+    appId: "1:712914050702:web:326ba26d42df0062cc33df"
+};
+
+const app = initializeApp(config);
