2d10fe52fcae28d3de6fd6a7f467a7c09948ff32
dhruv5gohel <dhruv5gohel@gmail.com>
2023-05-31 15:37:57 +0530

    Added 3 dots menu(Kebab) and in it added Edit room functionality

--- src/Chat-Window/ChatTop/ChatTop.js
+++ src/Chat-Window/ChatTop/ChatTop.js
@@ -1,4 +1,4 @@
-import { Button } from "rsuite";
+import { Button, Dropdown } from "rsuite";
 import { useCurrentRoom } from "../../context/CurrentRoom.context"
 import { memo } from "react";
 import RoomAvatar from "./RoomAvatar";
@@ -8,29 +8,44 @@ import { useMediaQuery } from "../../misc/custom-hooks";
 import { IoMdArrowRoundBack } from "react-icons/io"
 import { Icon } from "@rsuite/icons";
 import { Link } from "react-router-dom";
+import { GoKebabVertical } from "react-icons/go";
+import EditRoom from "./EditRoom";
+
+const renderButton = (props, ref) => {
+  return (
+    <Button {...props} ref={ref} color="red" appearance="primary" style={{ borderRadius: "0px", height: "100%" }}>
+      <Icon as={GoKebabVertical} />
+    </Button>
+  )
+}
 
 const ChatTop = () => {
   const name = useCurrentRoom(v => v.name);
   const [isOpen, setIsOpen] = useState(false);
+  const [editIsOpen, setEditIsOpen] = useState(false);
   const isMobile = useMediaQuery("(min-width: 992px)");
 
   return (
     <>
       <div>
-        <div className="d-flex" style={{width: "100%"}}>
+        <div className="d-flex" style={{ width: "100%" }}>
           <div className={!isMobile ? "d-flex" : "d-none"}>
             <Button as={Link} to="/">
               <Icon as={IoMdArrowRoundBack} />
             </Button>
           </div>
-          <div style={{width: "100%"}}>
-            <Button block style={{width: "100%"}} className="chat-top-main" color="red" appearance="primary" startIcon={<RoomAvatar name={name} />} onClick={() => { setIsOpen(p => !p) }}>
+          <div style={{ width: "100%" }} className="d-flex">
+            <Button block style={{ width: "100%" }} className="chat-top-main" color="red" appearance="primary" startIcon={<RoomAvatar name={name} />} onClick={() => { setIsOpen(p => !p) }}>
               <span style={{ fontSize: "1.5rem", fontWeight: "bolder", marginLeft: "10px" }}>{name}</span>
             </Button>
+            <Dropdown renderToggle={renderButton} placement="leftBottom">
+              <Dropdown.Item onClick={()=>setEditIsOpen(p => !p)}>Edit Room</Dropdown.Item>
+            </Dropdown>
           </div>
         </div>
       </div>
 
+      <EditRoom isOpen={editIsOpen} setIsOpen={setEditIsOpen} />
       <ChatTopModal isOpen={isOpen} setIsOpen={setIsOpen} name={name} />
     </>
   )
---
+++ src/Chat-Window/ChatTop/EditRoom.js
@@ -0,0 +1,49 @@
+import { Button, Modal } from "rsuite"
+import { useCurrentRoom } from "../../context/CurrentRoom.context"
+import EditableInput from "../../Components/EditableInput"
+import { ref, set } from "firebase/database"
+import { database } from "../../misc/firebase"
+import { toast } from "react-toastify"
+import { useParams } from "react-router"
+
+const EditRoom = ({ isOpen, setIsOpen }) => {
+    const name = useCurrentRoom(v => v.name)
+    const description = useCurrentRoom(v => v.description)
+    const { chatId } = useParams()
+
+    const updateData = (key, value) => {
+        set(ref(database, `/rooms/${chatId}/${key}`), value).then(()=>{
+            toast.success("Successfully Updated")
+        }).catch((err)=>{
+            toast.error(err.message)
+        })
+    }
+    console.log(isOpen)
+
+    const onNameSave = (newName) => {
+        updateData("name", newName)
+    }
+
+    const onDescSave = (newDesc) => {
+        updateData("description", newDesc)
+    }
+
+  return (
+    <Modal open={isOpen} onClose={()=>setIsOpen(p => !p)}>
+        <Modal.Header>
+            <Modal.Title>Edit Room</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <EditableInput initialValue={name} onSave={onNameSave} placeholder="Write Room name" emptyMsg="Room name cannot be empty" label={<h4>Name</h4>}/>
+            <EditableInput initialValue={description} as="textarea" rows={5} onSave={onDescSave} placeholder="Write Room name" emptyMsg="Room name cannot be empty" label={<h4>Name</h4>}/>
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button block onClick={() => setIsOpen(p => !p)}>Close</Button>
+        </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default EditRoom
